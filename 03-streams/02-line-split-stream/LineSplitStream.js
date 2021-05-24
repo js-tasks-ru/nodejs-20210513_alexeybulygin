9ba476c9b57f35ffbaf49b617772ab29849dd52c
Alexey Bulygin <ab@modumlab.com>
2021-05-24 17:30:09 +0300

    stream task 2

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,24 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+
+    this.encoding = options.encoding
   }
 
   _transform(chunk, encoding, callback) {
+    Buffer
+      .from(chunk, this.encoding)
+      .toString()
+      .split(os.EOL)
+      .forEach(part => {
+        this.push(part)
+      });
+
+    callback(null);
   }
 
   _flush(callback) {
+    callback(null);
   }
 }
 
